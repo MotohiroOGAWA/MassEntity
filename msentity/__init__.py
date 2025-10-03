c1919ca35260f2c4bf32046822816dd3edecfa63
MotohiroOGAWA <s213044y@st.go.tuat.ac.jp>
2025-10-03 05:38:00 +0000

    refactor: Replace direct imports with dynamic module loading for core, io, and utils

--- msentity/__init__.py
+++ msentity/__init__.py
@@ -1,3 +1,6 @@
-from . import core
-from . import io
-from . import utils
+# msentity/__init__.py
+import importlib
+
+core = importlib.import_module(".core", __package__)
+io = importlib.import_module(".io", __package__)
+utils = importlib.import_module(".utils", __package__)
