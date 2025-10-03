69b27e36f2a33edd9acfd02991a17b52503fea8f
MotohiroOGAWA <s213044y@st.go.tuat.ac.jp>
2025-10-03 05:54:07 +0000

    refactor: Switch to dynamic module loading for core, io, and utils; update pytest testpaths in configuration

--- msentity/__init__.py
+++ msentity/__init__.py
@@ -1,6 +1,8 @@
 # msentity/__init__.py
 import importlib
 
-core = importlib.import_module(".core", __package__)
-io = importlib.import_module(".io", __package__)
-utils = importlib.import_module(".utils", __package__)
+from . import core
+from . import io
+from . import utils
+
+__all__ = ["core", "io", "utils"]
